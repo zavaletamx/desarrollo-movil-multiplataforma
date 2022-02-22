f2e7f6279f97eff454f62b4018925bc5c7b71ce2
Raúl <raul.zavaletazea@gmail.com>
2022-02-22 12:41:00 -0600

    Ejemplo de implementación de click genérico en componentes visuales de ReactNative

--- src/screens/FlexBoxScreen.js
+++ src/screens/FlexBoxScreen.js
@@ -9,6 +9,23 @@ import {
 import colores from '../styles/colores';
 
 const FlexBoxScreen = (props) => {
+	/*
+    Creamos un state para poder 
+    actualizar en tiempo real el valor del 
+    display 
+    */
+	const [display, setDisplay] = React.useState('');
+	const [num1, setNum1] = React.useState(0);
+	const [num2, setNum2] = React.useState(0);
+	const [resultado, setResultado] = React.useState(0);
+
+	/*
+    Click genérico para pintar el display
+    */
+	const clickBoton = (texto) => {
+		setDisplay(`${display}${texto}`);
+	};
+
 	return (
 		<View
 			style={{
@@ -17,29 +34,114 @@ const FlexBoxScreen = (props) => {
 			}}>
 			<SafeAreaView style={estilo.contenedor}>
 				{/* Sección del display */}
-				<Text style={estilo.display}>0.01</Text>
+				<Text style={estilo.display}>
+					{display}
+				</Text>
 
 				{/* Panel de botones */}
 				<View style={estilo.panelBotones}>
 					<View style={estilo.fila}>
 						<TouchableOpacity
+							style={estilo.botonOperacion}
+							onPress={() => setDisplay('')}>
+							<Text style={estilo.textoBoton}>
+								AC
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilo.botonOperacion}
+							onPress={() => {
+								//Convertimos display a un valor double
+								const numeroDisplay =
+									parseFloat(display);
+
+								//Multiplicamos el valor por -1
+								setDisplay(
+									numeroDisplay * -1
+								);
+							}}>
+							<Text style={estilo.textoBoton}>
+								+/-
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
 							style={estilo.boton}>
+							<Text style={estilo.textoBoton}>
+								%
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilo.boton}>
+							<Text style={estilo.textoBoton}>
+								/
+							</Text>
+						</TouchableOpacity>
+					</View>
+
+					<View style={estilo.fila}>
+						<TouchableOpacity
+							style={estilo.boton}
+							onPress={() => clickBoton('7')}>
 							<Text style={estilo.textoBoton}>
 								7
 							</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity
-							style={estilo.boton}>
+							style={estilo.boton}
+							onPress={() => clickBoton('8')}>
 							<Text style={estilo.textoBoton}>
 								8
 							</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity
-							style={estilo.boton}>
+							style={estilo.boton}
+							onPress={() => clickBoton('9')}>
 							<Text style={estilo.textoBoton}>
 								9
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilo.botonOperacion}
+							onPress={() => {
+								console.log(display);
+								numeroDisplay =
+									parseFloat(display);
+
+								/*
+                                Evaluar si hay numeros 
+                                */
+							}}>
+							<Text style={estilo.textoBoton}>
+								+
+							</Text>
+						</TouchableOpacity>
+					</View>
+
+					<View style={estilo.fila}>
+						<TouchableOpacity
+							style={estilo.boton}>
+							<Text style={estilo.textoBoton}>
+								4
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilo.boton}>
+							<Text style={estilo.textoBoton}>
+								5
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilo.boton}>
+							<Text style={estilo.textoBoton}>
+								6
 							</Text>
 						</TouchableOpacity>
 
@@ -53,83 +155,24 @@ const FlexBoxScreen = (props) => {
 
 					<View style={estilo.fila}>
 						<TouchableOpacity
-							style={estilo.boton}>
+							style={estilo.boton}
+							onPress={() => clickBoton('1')}>
 							<Text style={estilo.textoBoton}>
-								7
+								1
 							</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity
 							style={estilo.boton}>
 							<Text style={estilo.textoBoton}>
-								8
+								2
 							</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity
 							style={estilo.boton}>
 							<Text style={estilo.textoBoton}>
-								9
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								+
-							</Text>
-						</TouchableOpacity>
-					</View>
-
-					<View style={estilo.fila}>
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								7
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								8
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								9
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								+
-							</Text>
-						</TouchableOpacity>
-					</View>
-
-					<View style={estilo.fila}>
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								7
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								8
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={estilo.boton}>
-							<Text style={estilo.textoBoton}>
-								9
+								3
 							</Text>
 						</TouchableOpacity>
 
@@ -198,6 +241,14 @@ const estilo = StyleSheet.create({
 	boton: {
 		flex: 1,
 		backgroundColor: colores.candyPink,
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 4,
+		borderRadius: 4,
+	},
+	botonOperacion: {
+		flex: 1,
+		backgroundColor: colores.yinMnBlue,
 		justifyContent: 'center',
 		alignItems: 'center',
 		margin: 4,
