5903584ab44c952ea3c2e002f7e9bd5f48236e2c
Raúl <raul.zavaletazea@gmail.com>
2022-03-15 13:42:46 -0600

    Ejemplo de consulta a firestore para mostrar marcadores en un mapa

--- src/screens/mapas/MapaScreen.js
+++ src/screens/mapas/MapaScreen.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { View } from 'react-native';
 /*
 Importamos la librería de mapas, previamente
@@ -8,7 +9,57 @@ import MapView, {
 	PROVIDER_GOOGLE,
 } from 'react-native-maps';
 
+import firebase from '../../backend/firebase';
+
 const MapaScreen = (props) => {
+	/*
+    Justo antes de mostrar el contenido de la ventana 
+    al momento de iniciar, creamos una consulta de nuestra colección
+    de ubicaciones para mostrarlos en el mapa
+
+    Cuando necesitamos una función asíncrona desde un efecto, es recomendable
+    crear una función adicional e invocarla en el efecto 
+    */
+	React.useEffect(() => {
+		cargarUbicaciones();
+	}, []);
+
+	/*
+    Estado para guardar el arreglo de ubicaciones
+    */
+	const [marcadores, setMarcadores] = React.useState([]);
+
+	const cargarUbicaciones = async () => {
+		const query = await firebase.database
+			.collection('ubicaciones')
+			.get();
+
+		const arrMarcadores = [];
+
+		//Recorremos los doscumentos de la coleccíon
+		query.docs.forEach((doc) => {
+			/*
+            Por cada documento de la colección
+            creamos un marcador 
+            */
+			arrMarcadores.push(
+				<MapView.Marker
+					key={doc.id}
+					coordinate={{
+						latitude: doc.data().punto.latitude,
+						longitude:
+							doc.data().punto.longitude,
+					}}
+					description={doc.data().nombre}
+					title={'Marcador'}
+				/>
+			);
+		});
+
+		//pasamos el arreglo de marcadores al state
+		setMarcadores(arrMarcadores);
+	};
+
 	return (
 		<View style={{ flex: 1 }}>
 			<MapView
@@ -28,8 +79,8 @@ const MapaScreen = (props) => {
 				initialRegion={{
 					latitude: 20.7482135,
 					longitude: -99.9573658,
-					latitudeDelta: 0.3,
-					longitudeDelta: 0.3,
+					latitudeDelta: 0.5,
+					longitudeDelta: 0.5,
 				}}>
 				{/* 
                 Agregamos un marcador 
@@ -40,6 +91,7 @@ const MapaScreen = (props) => {
                 título
                 */}
 				<MapView.Marker
+					key={'m-0'}
 					coordinate={{
 						latitude: 20.7482135,
 						longitude: -99.9573658,
@@ -47,6 +99,8 @@ const MapaScreen = (props) => {
 					title='Peña de Bernal'
 					description='La maravillosa y encantadora Peña de Bernal, con sus deliciosas licuachelas y su variedad de pan de queso, nieve artesanal, gorditas de nata y sus tradicionales tipos de esquites'
 				/>
+				{/* Mostramos el arreglo de marcadores */}
+				{marcadores}
 			</MapView>
 		</View>
 	);
